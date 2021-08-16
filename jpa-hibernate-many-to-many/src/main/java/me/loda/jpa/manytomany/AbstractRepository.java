214e1bdd3a0697154c97a3f6181805aec4273670
Đinh Thế Luân <luandt1005@gmail.com>
2021-08-16 12:22:31 +0700

    LuanDT

--- jpa-hibernate-many-to-many/src/main/java/me/loda/jpa/manytomany/AbstractRepository.java
+++ jpa-hibernate-many-to-many/src/main/java/me/loda/jpa/manytomany/AbstractRepository.java
@@ -1,11 +1,10 @@
 package me.loda.jpa.manytomany;
 
-import java.io.Serializable;
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.NoRepositoryBean;
+
+import java.io.Serializable;
 
 /**
  * Copyright 2019 {@author Loda} (https://loda.me).
@@ -17,6 +16,4 @@ import org.springframework.data.repository.NoRepositoryBean;
 @NoRepositoryBean
 public interface AbstractRepository<T, ID extends Serializable> extends JpaRepository<T, ID>,
                                                                         JpaSpecificationExecutor<T>{
-
-    T findById(ID id);
 }
